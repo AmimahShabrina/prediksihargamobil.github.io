42c3cf8810fa2ab1eba4329bb2a3a236ce95c03c
AmimahShabrina <93334321+AmimahShabrina@users.noreply.github.com>
2022-01-10 01:08:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, request
-import os
+import jsonify
 import requests
 import pickle
 import numpy as np
 import sklearn
 from sklearn.preprocessing import StandardScaler
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__)
 model = pickle.load(open('random_forest_regression_model.pkl', 'rb'))
 
 @app.route('/',methods=['GET'])
@@ -47,7 +47,7 @@ def predict():
         if output<0:
             return render_template('index.html',prediction_texts="Maaf Anda tidak dapat menjual mobil ini")
         else:
-            return render_template('index.html',prediction_text="Anda dapat menjual mobil di harga {} lakhs".format(output))
+            return render_template('index.html',prediction_text="Anda dapat menjual mobil di harga {}".format(output))
     else:
         return render_template('index.html')
 
